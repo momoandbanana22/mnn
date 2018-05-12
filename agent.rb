3b67d58a891ac4e4fab96f163c9f28d1218b6712
momoandbanana22 <momoandbanana@protonmail.com>
2018-05-12 22:09:18 +0900

    sleep入れた

--- agent.rb
+++ agent.rb
@@ -255,7 +255,7 @@ class Agent
   public def baibai
     @mythread = Thread.start do
       loop do
-        # sleep(0.000001)
+        sleep(0.000001) # 1uS
         if @to_stop && !@stopped
           disp = "#{DateTime.now} #{object_id} #{@target_pair} stopping..."
           puts(disp)
